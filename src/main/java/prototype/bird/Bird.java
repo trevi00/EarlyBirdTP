1b0b91dbea8e9d69dda2b4ce9eb6c84516c5ca1e
trevi00 <gohardks00@gmail.com>
2025-05-14 15:20:16 +0900

    확인 후 프로토타입 디렉터리로 옮김

---
+++ src/main/java/prototype/bird/Bird.java
@@ -0,0 +1,29 @@
+package prototype.bird;
+
+public class Bird {
+ 
+    private BirdStatus stage;
+    private int point;
+    
+    public Bird(){
+        this.stage= BirdStatus.EGG;
+        this.point=0;
+    }
+    
+    public BirdStatus getStage() {
+        return stage;
+    }
+    
+    public void setStage(BirdStatus stage) {
+        this.stage = stage;
+    }
+    
+    public int getPoint() {
+        return point;
+    }
+    
+    public void setPoint(int point) {
+        this.point = point;
+    }
+    
+}
