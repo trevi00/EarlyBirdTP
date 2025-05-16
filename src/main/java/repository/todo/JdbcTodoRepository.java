ff7b91960ec091cdca53d3030df18611a9f05d2e
hwShin0727 <hwshin0727@gmail.com>
2025-05-16 18:21:05 +0900

    유저, 투두 레포지토리 작업 중

--- src/main/java/repository/todo/JdbcTodoRepository.java
+++ src/main/java/repository/todo/JdbcTodoRepository.java
@@ -1,11 +1,43 @@
 package repository.todo;
 
+import Entity.Todo;
 import database.DatabaseConnection;
 
 import java.sql.Connection;
+import java.time.LocalDate;
+import java.util.List;
 
 public class JdbcTodoRepository implements TodoRepository {
 
     final Connection connection = DatabaseConnection.getConnection();
 
+    @Override
+    public void save(Todo todo) {
+
+    }
+
+    @Override
+    public Todo findByUsernameAndDate(String username, LocalDate date) {
+        return null;
+    }
+
+    @Override
+    public List<Todo> findAllByUsername(String username) {
+        return List.of();
+    }
+
+    @Override
+    public void delete(String username, LocalDate date) {
+
+    }
+
+    @Override
+    public boolean exists(String username, LocalDate date) {
+        return false;
+    }
+
+    @Override
+    public void markAsDone(String username, LocalDate date) {
+
+    }
 }
