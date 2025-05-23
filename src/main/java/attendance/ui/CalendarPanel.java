5184b19ba49d1d51268d7b087a03a2858e5f9601
hwShin0727 <hwshin0727@gmail.com>
2025-05-23 11:58:15 +0900

    메인메뉴 배치 조정중

--- src/main/java/attendance/ui/CalendarPanel.java
+++ src/main/java/attendance/ui/CalendarPanel.java
@@ -1,7 +1,11 @@
 package attendance.ui;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.URL;
 import java.time.LocalDate;
 import java.time.YearMonth;
 import java.util.Set;
@@ -15,33 +19,41 @@ public class CalendarPanel extends JPanel {
     private final YearMonth yearMonth;
     private final Set<LocalDate> attendanceDays;
     private ImageIcon checkIcon = null;
+    private BufferedImage background = null;
 
     public CalendarPanel(YearMonth yearMonth, Set<LocalDate> attendanceDays) {
         this.yearMonth = yearMonth;
         this.attendanceDays = attendanceDays;
         try {
-            Image img = new ImageIcon(getClass().getResource("/img/출석확인/check.png")).getImage();
+            Image img = new ImageIcon(getClass().getResource("/img/출석현황/check.png")).getImage();
             Image scaled = img.getScaledInstance(35, 35, Image.SCALE_SMOOTH);
             checkIcon = new ImageIcon(scaled);
+            URL imageURL = getClass().getResource("/img/출석현황/bg_brown.png");
+            background = ImageIO.read(imageURL);
         } catch (Exception e) {
-            System.err.println("❌ check.png 이미지 로드 실패: " + e.getMessage());
+            System.err.println("이미지 로드 실패: " + e.getMessage());
         }
         setLayout(new GridLayout(0, 7)); // 7열 (일~토)
+
         drawCalendar();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (background != null) {
+            // Draw image to fill the entire panel
+            g.drawImage(background, 0, 0, getWidth(), getHeight(), this);
+        }
     }
 
     private void drawCalendar() {
         // 요일 헤더
-        String[] weekDays = {"일", "월", "화", "수", "목", "금", "토"};
+        String[] weekDays = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
         for (int i = 0; i < 7; ++i) {
             String day = weekDays[i];
             JLabel lbl = new JLabel(day, SwingConstants.CENTER);
-            lbl.setFont(lbl.getFont().deriveFont(Font.BOLD));
-            if(i == 0) {
-                lbl.setForeground(Color.RED);
-            } else if(i == 6) {
-                lbl.setForeground(Color.BLUE);
-            }
+            lbl.setFont(lbl.getFont().deriveFont(Font.BOLD).deriveFont(15f));
             add(lbl);
         }
 
@@ -69,6 +81,7 @@ public class CalendarPanel extends JPanel {
             } else {
                 // 일반 날짜 숫자
                 label = new JLabel(String.valueOf(day), SwingConstants.CENTER);
+                label.setFont(label.getFont().deriveFont(Font.BOLD).deriveFont(15f));
             }
 
             int currentDay = (7 + day - emptyStart) % 7;
--- src/main/java/attendance/ui/FrameAttendanceStats.java
+++ src/main/java/attendance/ui/FrameAttendanceStats.java
@@ -15,12 +15,13 @@ public class FrameAttendanceStats extends JFrame {
 
     private final AttendanceStatsService statsService;
     private final String username;
+    private JLabel bg = null;
 
     public FrameAttendanceStats(AttendanceStatsService statsService, String username) {
         this.statsService = statsService;
         this.username = username;
 
-        setTitle("출석기록");
+        setTitle("출석현황");
         setSize(400, 400);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
@@ -37,11 +38,13 @@ public class FrameAttendanceStats extends JFrame {
         // ✅ 날짜 포맷을 "yyyy-MM" 형식으로 맞추어 전달
         YearMonth yearMonth = YearMonth.now();
         String formatted = yearMonth.format(DateTimeFormatter.ofPattern("yyyy-MM"));
-
+        char[] monthChars = yearMonth.getMonth().toString().toCharArray();
+        for(int i = 1; i < monthChars.length; ++i) {
+            monthChars[i] = Character.toLowerCase(monthChars[i]);
+        }
         JPanel monthPanel = new JPanel();
         String monthText = "<html><div style='text-align:center;'>" +
-                yearMonth.format(DateTimeFormatter.ofPattern("M")) + " 월"
-                + "</div></html>";
+                String.valueOf(monthChars) + "</div></html>";
         JLabel monthLabel = new JLabel(monthText);
         monthLabel.setFont(monthLabel.getFont().deriveFont(Font.BOLD));
         monthPanel.add(monthLabel, SwingConstants.CENTER);
